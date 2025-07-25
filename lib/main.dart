55b76564d62cb22bda06b7756689e2d3d5243c3e
agney <agneysuresh@gmail.com>
2025-07-26 00:12:00 +0530

    version 0.1 final

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WeatherPage()
+      theme: ThemeData(
+        fontFamily: 'Poppins'
+      ),
+      home: const WeatherPage()
     );
   }
 }
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -9,7 +9,6 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _weatherPageState extends State<WeatherPage> {
-
   final _weatherService = WeatherService('6868bc1fa5ce3bf1f1ec93080061b549');
   Weather? _weather;
 
@@ -21,31 +20,115 @@ class _weatherPageState extends State<WeatherPage> {
       setState(() {
         _weather = weather;
       });
-
-    } 
-    catch (e) {
+    } catch (e) {
       print(e);
     }
   }
+
+  String getWeatherIcon(String? condition) {
+    if (condition == null) return 'assets/none.png';
+
+    switch (condition.toLowerCase()) {
+      case 'clouds':
+        return 'assets/clouds.png';
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/fog.png';
+      case 'rain':
+        return 'assets/rain.png';
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/drizzle.png';
+      case 'thunderstorm':
+        return 'assets/thunder.png';
+      case 'clear':
+        return 'assets/sunny.png';
+      default:
+        return 'assets/sunny.png';
+    }
+  }
+
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
     _fetchWeather();
   }
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(_weather?.cityName ?? "Loading city..."),
-              Text('${_weather?.temperature ?? "--"}°C'),
-            ],
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Tenki.',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Color(0xFF343A40),
+            fontWeight: FontWeight.bold,
+            fontSize: 36
           ),
-        )
-      );
-    }
+        ),
+        backgroundColor: Color(0xffe9ecef), // same as your app bg
+        elevation: 0,
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Color(0xFF343A40)),
+      ),
+      backgroundColor: Color(0xffe9ecef),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0, top: 0.0),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Transform.translate(
+                  offset: Offset(0, -30), // shift up by 20 pixels
+                  child: Text(
+                    _weather?.cityName ?? "Loading city...",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff6c757d),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Image.asset(
+                getWeatherIcon(_weather?.condition),
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.contain,
+              ),
+            ),
+
+            Text(
+              '${_weather?.temperature.round() ?? "--"}°C',
+              style: TextStyle(
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff343a40),
+              ),
+            ),
+
+            Text(
+              _weather?.condition ?? "",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff6c757d),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
